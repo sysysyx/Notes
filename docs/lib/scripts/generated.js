6f95f3be29e62197de7870b0f681f86563f6f04c
sunast <jlsynjsyx@163.com>
2024-07-18 23:55:01 +0800

    test

--- docs/lib/scripts/generated.js
+++ docs/lib/scripts/generated.js
@@ -1,1 +1,1 @@
-let nodes={paths:[],nodeCount:0,linkSources:[],linkTargets:[],labels:[],radii:[],linkCount:0},attractionForce=1,linkLength=10,repulsionForce=150,centralForce=3,edgePruning=100
+let nodes={paths:["index.html"],nodeCount:1,linkSources:[],linkTargets:[],labels:["index"],radii:[null],linkCount:0},attractionForce=1,linkLength=10,repulsionForce=150,centralForce=3,edgePruning=100
